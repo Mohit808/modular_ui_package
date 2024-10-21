c32040cdfdaf2a190df1fd81c0a1bcfedf62092b
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2024-10-21 13:21:48 +0530

    Update category_widget.dart

--- lib/common_widget/category_widget.dart
+++ lib/common_widget/category_widget.dart
@@ -6,17 +6,18 @@ import '../text_atoms/text_common.dart';
 import 'container_decorated.dart';
 
 class CategoryWidget extends StatelessWidget {
-  const CategoryWidget({super.key, this.height, this.width,this.image,this.text});
+  const CategoryWidget({super.key, this.height, this.width,this.image,this.text,this.loading});
   final double? height;
   final double? width;
   final String? image;
   final String? text;
+  final bool? loading;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ContainerDecorated(color: Colors.pinkAccent.shade100.withOpacity(0.1),borderRadius: 15,child: NetworkOrAssetImage(src: image!=null?image! : DateTime.timestamp().millisecond.isEven?AppImagesConst.vegitable2: AppImagesConst.vegitable,height: height??75,width: width??75,fit: BoxFit.cover,),),
+        ContainerDecorated(color: loading!=null?null:Colors.pinkAccent.shade100.withOpacity(0.1),borderRadius: 15,child: NetworkOrAssetImage(src: image!=null?image! : DateTime.timestamp().millisecond.isEven?AppImagesConst.vegitable2: AppImagesConst.vegitable,height: height??75,width: width??75,fit: BoxFit.cover,),),
         const SizedBox(height: 8,),
         SizedBox(width: 75,child: SmallText(text: text??"Fresh \nVegitable",fontWeight: FontWeight.w700,textAlign: TextAlign.center,))
       ],
