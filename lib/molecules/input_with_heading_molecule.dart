007947143666c86aa645c5235a47daed3fa18816
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2024-11-18 22:25:01 +0530

    Update input_with_heading_molecule.dart

--- lib/molecules/input_with_heading_molecule.dart
+++ lib/molecules/input_with_heading_molecule.dart
@@ -4,11 +4,12 @@ import '../text_atoms/text_common.dart';
 import '../theme/app_sizes.dart';
 
 class InputWithHeadingMolecule extends StatelessWidget{
-  const InputWithHeadingMolecule({super.key, required this.heading, this.mandatoryString, required this.widget, this.colorMandatoryString});
+  const InputWithHeadingMolecule({super.key, required this.heading, this.mandatoryString, required this.widget, this.colorMandatoryString,this.fontWeightHeading});
   final dynamic heading;
   final String? mandatoryString;
   final Widget widget;
   final Color? colorMandatoryString;
+  final FontWeight? fontWeightHeading;
 
 
   @override
@@ -17,8 +18,8 @@ class InputWithHeadingMolecule extends StatelessWidget{
       const SizedBox(height: AppSizes.h_16),
       heading.runtimeType!=String?heading: Row(
         children: [
-          SmallText(text: "$heading",),
-          mandatoryString==null?SizedBox():SmallText(text: " $mandatoryString",color: colorMandatoryString,),
+          SmallText(text: "$heading",fontWeight: fontWeightHeading,),
+          mandatoryString==null?SizedBox():SmallText(text: " $mandatoryString",color: colorMandatoryString,fontWeight: fontWeightHeading,),
         ],
       ),
       const SizedBox(height: AppSizes.h_8),
