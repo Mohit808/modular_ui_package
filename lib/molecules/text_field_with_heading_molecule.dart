eeb8a281bdaa1b1959719970641145d8c5b01e88
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2024-11-18 22:44:32 +0530

    Update text_field_with_heading_molecule.dart

--- lib/molecules/text_field_with_heading_molecule.dart
+++ lib/molecules/text_field_with_heading_molecule.dart
@@ -5,7 +5,7 @@ import 'input_with_heading_molecule.dart';
 import '../common_widget/textfield_atom.dart';
 
 class TextFieldWithHeadingMolecule extends StatelessWidget {
-  const TextFieldWithHeadingMolecule({super.key, this.controller, this.hintText, this.validator, this.inputFormatters, this.keyboardType, this.onChanged, this.heading, this.prefixIcon, this.mandatoryString, this.colorMandatoryString, this.suffixIcon,this.fontWeightHeading});
+  const TextFieldWithHeadingMolecule({super.key, this.controller, this.hintText, this.validator, this.inputFormatters, this.keyboardType, this.onChanged, this.heading, this.prefixIcon, this.mandatoryString, this.colorMandatoryString, this.suffixIcon,this.fontWeightHeading,this.maxLines});
   final TextEditingController? controller;
   final String? hintText;
   final String? Function(String?)? validator;
@@ -18,10 +18,11 @@ class TextFieldWithHeadingMolecule extends StatelessWidget {
   final Color? colorMandatoryString;
   final Widget? suffixIcon;
   final FontWeight? fontWeightHeading;
+  final int? maxLines;
 
 
   @override
   Widget build(BuildContext context) {
-    return  InputWithHeadingMolecule(fontWeightHeading: fontWeightHeading,mandatoryString: mandatoryString,colorMandatoryString: colorMandatoryString,heading: heading??"", widget: TextFieldCommon(suffixIcon: suffixIcon,prefixIcon: prefixIcon,controller: controller,hintText: hintText??"",validator: validator,inputFormatters: inputFormatters,keyboardType: keyboardType,onChanged: onChanged,));
+    return  InputWithHeadingMolecule(fontWeightHeading: fontWeightHeading,mandatoryString: mandatoryString,colorMandatoryString: colorMandatoryString,heading: heading??"", widget: TextFieldCommon(maxLines: maxLines,suffixIcon: suffixIcon,prefixIcon: prefixIcon,controller: controller,hintText: hintText??"",validator: validator,inputFormatters: inputFormatters,keyboardType: keyboardType,onChanged: onChanged,));
   }
 }
