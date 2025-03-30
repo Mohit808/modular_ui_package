bae53e3e9b14883400892ea32f991b64845e2719
Mohit808 <amityadav.ayx@gmail.com>
2025-03-30 19:32:32 +0530

    ..

--- lib/common_widget/clear_cart_widget.dart
+++ lib/common_widget/clear_cart_widget.dart
@@ -3,17 +3,22 @@ import 'package:flutter/material.dart';
 import '../text_atoms/text_common.dart';
 
 class ClearCartWidget extends StatelessWidget {
-  const ClearCartWidget({super.key,this.hideClearBtn});
+  const ClearCartWidget({super.key,this.hideClearBtn, this.onTap});
   final dynamic hideClearBtn;
+  final Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-      HeadingText(text: "Your Cart",letterSpacing: 0.5,color: Colors.black,),
-      hideClearBtn!=null?SizedBox():Row(
-        children: [
-          SmallText(text: "Clear cart  ",letterSpacing: 0.5,color: Colors.black54,fontWeight: FontWeight.w700,size: 13,),
-          Icon(Icons.delete_rounded,size: 14,)
-        ],
+      const HeadingText(text: "Your Cart",letterSpacing: 0.5,color: Colors.black,),
+      hideClearBtn!=null?const SizedBox():InkWell(onTap: (){
+        if(onTap!=null) onTap!();
+      },
+        child: const Row(
+          children: [
+            SmallText(text: "Clear cart  ",letterSpacing: 0.5,color: Colors.black54,fontWeight: FontWeight.w700,size: 13,),
+            Icon(Icons.delete_rounded,size: 14,)
+          ],
+        ),
       ),
     ],);
 
