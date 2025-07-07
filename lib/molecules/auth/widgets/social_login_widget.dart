01709cbfac7f7d707b10bc76cc2d2fb9b550efe6
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2025-07-07 21:11:32 +0530

    google signin

--- lib/molecules/auth/widgets/social_login_widget.dart
+++ lib/molecules/auth/widgets/social_login_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:modular_ui_package/molecules/auth/widgets/social_login/controller_social_login.dart';
 import 'package:modular_ui_package/utils/app_images.dart';
+import 'package:modular_ui_package/utils/network_handling/api_response.dart';
 
 import '../../../common_functions/image_network.dart';
 import '../../../common_widget/button_single_atom.dart';
@@ -9,9 +10,10 @@ import '../../../text_atoms/text_common.dart';
 
 
 class SocialLoginWidget extends StatelessWidget {
-  const SocialLoginWidget({super.key, this.onTapGoogle, this.hideAppleLogin});
-  final Function()? onTapGoogle;
+  const SocialLoginWidget({super.key, this.callBack, this.hideAppleLogin, this.apiResponseGoogle});
+  final Function(dynamic)? callBack;
   final bool? hideAppleLogin;
+  final ApiResponse? apiResponseGoogle;
 
 
   @override
@@ -20,13 +22,13 @@ class SocialLoginWidget extends StatelessWidget {
       builder: (controller) {
         return Column(spacing: 24,children: [
 
-          ButtonSingleAtom(color: Colors.white,colorBorder: Colors.grey.shade400,widget: Row(mainAxisAlignment: MainAxisAlignment.center,children: [
+          ButtonSingleAtom(status:apiResponseGoogle?.status,color: Colors.white,colorBorder: Colors.grey.shade400,widget: Row(mainAxisAlignment: MainAxisAlignment.center,children: [
             ImageCommon(src: AppImagesConst.google),
             const SizedBox(width: 16,),
             const NormalHeadingText(text: "Continue with Google",color: Colors.grey,)
           ],),tap: () async {
-            // dynamic value=await controller.loginWithGoogle();
-            if(onTapGoogle!=null) onTapGoogle!();
+            dynamic value=await controller.loginWithGoogle();
+            if(callBack!=null) callBack!(value);
           },),
 
 
