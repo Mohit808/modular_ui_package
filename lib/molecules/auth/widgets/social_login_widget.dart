492e2ecfa86133b12fcc192c4c45a4057c313d0f
Amit Yadav <35875197+Mohit808@users.noreply.github.com>
2026-02-04 11:33:15 +0530

    Update social login widget to use FontAwesome icons
    Replaced ImageCommon with FontAwesome icons for Google and Apple login buttons.

--- lib/molecules/auth/widgets/social_login_widget.dart
+++ lib/molecules/auth/widgets/social_login_widget.dart
@@ -7,7 +7,7 @@ import 'package:modular_ui_package/utils/network_handling/api_response.dart';
 import '../../../common_functions/image_network.dart';
 import '../../../common_widget/button_single_atom.dart';
 import '../../../text_atoms/text_common.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SocialLoginWidget extends StatelessWidget {
   const SocialLoginWidget({super.key, this.callBack, this.hideAppleLogin, this.apiResponseGoogle});
@@ -23,7 +23,8 @@ class SocialLoginWidget extends StatelessWidget {
         return Column(spacing: 24,children: [
 
           ButtonSingleAtom(status:apiResponseGoogle?.status,color: Colors.white,colorBorder: Colors.grey.shade400,widget: Row(mainAxisAlignment: MainAxisAlignment.center,children: [
-            ImageCommon( AppImagesConst.google),
+            // ImageCommon( AppImagesConst.google),
+            FaIcon(FontAwesomeIcons.google),
             const SizedBox(width: 16,),
             const NormalHeadingText(text: "Continue with Google",color: Colors.grey,)
           ],),tap: () async {
@@ -33,7 +34,8 @@ class SocialLoginWidget extends StatelessWidget {
 
 
           if(hideAppleLogin!=true)ButtonSingleAtom(color: Colors.white,colorBorder: Colors.grey.shade400,widget: Row(mainAxisAlignment: MainAxisAlignment.center,children: [
-            ImageCommon( AppImagesConst.apple),
+            // ImageCommon( AppImagesConst.apple),
+            FaIcon(FontAwesomeIcons.apple),
             const SizedBox(width: 16,),
             const NormalHeadingText(text: "Continue with Apple",color: Colors.grey,)
           ],),),
